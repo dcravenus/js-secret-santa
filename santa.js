e4d83eaec16ed5af6b39417fbe2d43af69bd0cd4
Derrick Scott Craven <dcravenus@gmail.com>
2017-04-07 13:27:34 -0600

    Add shuffle and random functions

--- santa.js
+++ santa.js
@@ -14,7 +14,22 @@ function santa(people) {
 }
 
 function shuffle(list) {
+  for(var i=0; i<list.length; i++){
+    //generate random number such that i <= j < n
+    var j = getRandomInt(i, list.length);
+    //exchange list[i] and list[j]
+    var temp = list[i];
+    list[i] = list[j];
+    list[j] = temp;
+  }
+
   return list;
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max-min)) + min;
 }
 
 var people = ['John', 'George', 'Paul', 'Ringo'];
